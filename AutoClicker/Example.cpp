9e3151eb34535db556803735d3d280ab7e381d59
Segward <gustask@stud.ntnu.no>
2024-11-23 04:42:21 +0100

    Random time delay

--- AutoClicker/Example.cpp
+++ AutoClicker/Example.cpp
@@ -1,17 +1,27 @@
 #include "../macmacro.hpp"
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
+
+// Function to generate a random delay between 100 milliseconds and 2 seconds
+unsigned int getRandomDelay() {
+    return 100000 + (rand() % (2000000 - 100000 + 1));
+}
 
 // Main function
 int main() {
+    // Seed the random number generator
+    srand(time(NULL));
 
     // Delay for 3 seconds
     usleep(3000000); // Sleep for 3 seconds
 
     while (true) {
-
         // Simulate mouse click
         SimulateMouseClick();
-        usleep(100000); // Sleep for 100 ms
 
+        // Get a random delay between 100 milliseconds and 2 seconds
+        unsigned int delay = getRandomDelay();
+        usleep(delay); // Sleep for the random delay
     }
 
     return 0;
