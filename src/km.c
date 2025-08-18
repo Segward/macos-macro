1f01cb58225a79f0ff528ba7a798c28f7b66ca11
Segward <gustask@stud.ntnu.no>
2025-08-18 15:49:42 +0200

    Made km do skills

--- src/km.c
+++ src/km.c
@@ -11,19 +11,44 @@ int main(int argc, char *argv[]) {
   printf("App started\n");
 
   CGKeyCode keyCodeC = 8;
-  unsigned int threshold = 1000000;
-  unsigned int min = threshold - 1000;
-  unsigned int max = threshold + 50000;
+  CGKeyCode keyCodeV = 9;
+  CGKeyCode keyCode1 = 18;
+  CGKeyCode keyCode2 = 19;
+  CGKeyCode keyCode3 = 20;
+  CGKeyCode keyCode4 = 21;
+
+  unsigned int threshold = 20000;
+  unsigned int min = threshold - 5000;
+  unsigned int max = threshold + 5000;
 
   int i = 0;
   while (true) {
-    if (i % 100 == 0) {
+    if (i % 1000 == 0) {
       SimulateKeyPress(keyCodeC);
       usleep(3000000);
       SimulateKeyRelease(keyCodeC);
     }
 
-    SimulateMouseClick();
+    if (i % 10) {
+      SimulateKeyPress(keyCode1);
+      usleep(1000);
+      SimulateKeyRelease(keyCode1);
+      SimulateKeyPress(keyCode2);
+      usleep(1000);
+      SimulateKeyRelease(keyCode2);
+      SimulateKeyPress(keyCode3);
+      usleep(1000);
+      SimulateKeyRelease(keyCode3);
+      SimulateKeyPress(keyCode4);
+      usleep(1000);
+      SimulateKeyRelease(keyCode4);
+      usleep(1000);
+    }
+
+    SimulateKeyPress(keyCodeV);
+    usleep(1000);
+    SimulateKeyRelease(keyCodeV);
+
     unsigned int delay = getRandomDelay(min, max);
     printf("Next click in %u microseconds\n", delay);
     usleep(delay);
